bd7d9da119d48e1f99ce1f28c1f03579ecd3b04b
strarsis <strarsis@gmail.com>
2018-06-12 19:22:28 +0200

    Update Controls.php

--- Controls.php
+++ Controls.php
@@ -137,6 +137,25 @@ class Controls {
 		}
 	}
 
+	/**
+	 * Returns an accessibility-friendly link to edit a post or page.
+	 *
+	 * This also gives us a little context about what exactly we're editing
+	 * (post or page?) so that users understand a bit more where they are in terms
+	 * of the template hierarchy and their content. Helpful when/if the single-page
+	 * layout with multiple posts/pages shown gets confusing.
+	 */
+	public static function edit_link() {
+		edit_post_link(
+			sprintf(
+				/* translators: %s: Name of current post */
+				__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
+				get_the_title()
+			),
+			'<span class="edit-link">',
+			'</span>'
+		);
+	}
 
 	public static function get_lib_url() {
 		$prefix = '/vendor/strarsis/sage9-onepager-lib';
